b24c43ba58d8f046af58bd6c82938d29959df8e3
jimini <gjm0313@naver.com>
2024-03-18 17:11:24 +0900

    create boardUser entity

--- src/board/entities/board.entity.ts
+++ src/board/entities/board.entity.ts
@@ -1,1 +1,31 @@
-export class Board {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({
+  name: 'board',
+})
+export class Board {
+  @PrimaryGeneratedColumn()
+  id: bigint;
+
+  @Column({ type: 'varchar', nullable: false })
+  name: string;
+
+  @Column({ type: 'varchar', nullable: false })
+  color: string;
+
+  @Column({ type: 'text', nullable: false })
+  info: string;
+
+  @Column({ type: 'text', nullable: false })
+  columnOrder: string;
+
+  @Column({
+    type: 'datetime',
+    default: () => 'CURRENT_TIMESTAMP',
+    nullable: false,
+  })
+  createdAt: Date;
+
+  @Column({ type: 'datetime', nullable: true })
+  updatedAt?: Date;
+}
