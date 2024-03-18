076dfb618338d8fa385db0a12b389f7a756db0eb
ratempty <rkdekqls258@naver.com>
2024-03-18 17:04:31 +0900

    test :  1차 커밋

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,24 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ColumnModule } from './column/column.module';
+<<<<<<< Updated upstream
 
 @Module({
   imports: [ColumnModule],
+=======
+<<<<<<< Updated upstream
+import { BoardModule } from './board/board.module';
+import { CommentModule } from './comment/comment.module';
+
+@Module({
+  imports: [ColumnModule, BoardModule],CommentModule,
+=======
+import { CommentModule } from './comment/comment.module';
+
+@Module({
+  imports: [ColumnModule, CommentModule],
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
   controllers: [AppController],
   providers: [AppService],
 })
