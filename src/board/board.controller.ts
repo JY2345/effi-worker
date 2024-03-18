89a6ff1904cd02c9c472d74bc4e5a0a07e483013
jimini <gjm0313@naver.com>
2024-03-19 07:08:28 +0900

    feat: Patch Board

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -12,6 +12,7 @@ import { BoardService } from './board.service';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
 import { AuthGuard } from '@nestjs/passport';
+import { User } from 'src/user/entities/user.entity';
 
 // jwt로 인증하기 @UseGuard(AuthGuard('jwt'))
 @Controller('board')
@@ -21,16 +22,40 @@ export class BoardController {
   // 인증받은 유저 데려오기 @UserInfo() user:User
   @Post()
   async create(@Body() createBoardDto: CreateBoardDto) {
-    return await this.boardService.create(createBoardDto);
+    const board = await this.boardService.create(createBoardDto);
+    return {
+      successMessage: '보드 생성에 성공하였습니다.',
+      board,
+    };
   }
 
   // 보드 조회
   @Get()
   async findAll() {
-    return await this.boardService.findAll();
+    const boards = await this.boardService.findAll();
+    return {
+      successMessage: '보드 조회에 성공하였습니다.',
+      boards,
+    };
   }
 
   // 보드 수정
+  @Patch(':id')
+  async update(
+    @Param('id') id: bigint,
+    /*@UserInfo() user:User, */ createBoardDto: CreateBoardDto,
+  ) {
+    //이건 지울것
+    const user = {
+      id: 1,
+    };
+
+    const board = await this.boardService.update(id, user.id, createBoardDto);
+    return {
+      successMessage: '보드 수정에 성공하였습니다.',
+      board,
+    };
+  }
 
   // 보드 삭제
 
--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { CreateBoardDto } from './dto/create-board.dto';
 import { UpdateBoardDto } from './dto/update-board.dto';
 import { InjectRepository } from '@nestjs/typeorm';
@@ -35,12 +35,33 @@ export class BoardService {
   }
 
   // 보드 수정
-  async update(createBoardDto: CreateBoardDto, userId: User) {
+  async update(id: bigint, userId: number, createBoardDto: CreateBoardDto) {
     // userTable의 findById같은 아이디 찾는것 가져오기
-    // const user = await this.userService.findbyId(userId)
+    const { name, color, info } = createBoardDto;
+    const board = await this.findById(id);
+    if (board.userId !== userId) {
+      throw new ForbiddenException('수정할 권한이 없습니다.');
+    }
+
+    const updateBoard = await this.boardRepository.save({
+      name,
+      color,
+      info,
+    });
+
+    return updateBoard;
   }
 
   // 보드 삭제
 
   // 보드 초대
+
+  // 보드 아이디 검색 함수
+  async findById(id: bigint) {
+    const board = await this.boardRepository.findOne({
+      where: { id },
+    });
+
+    return board;
+  }
 }
