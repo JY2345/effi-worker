11a82500890014814b3a358e70ff4b031bd23f77
HA0N1 <intor48@gmail.com>
2024-03-18 17:34:16 +0900

    1차

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { UserModule } from 'src/user/user.module';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt', session: false }), // jwt사용
+    JwtModule.registerAsync({
+      useFactory: (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET_KEY'),
+        signOptions: { expiresIn: '1h' },
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+  ],
+  providers: [JwtStrategy],
+})
+export class AuthModule {}
--- src/user/entities/user.entity.ts
+++ src/user/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 import { BoardUser } from 'src/board/entities/boardUser.entity';
+import { Comment } from 'src/comment/entities/comment.entity';
 import {
   Column,
   CreateDateColumn,
@@ -41,6 +42,9 @@ export class User {
 
   @OneToMany(() => BoardUser, (boardUser) => boardUser.user)
   boardUser: BoardUser[];
+
+  @OneToMany(() => Comment, (comment) => comment.user)
+  comment: Comment[];
 
   @OneToMany(() => Task, (task) => task.user)
   task: Task[];
