48e33e69d17b55e3d52bde72eccd457efb85450a
ratempty <rkdekqls258@naver.com>
2024-03-19 18:30:14 +0900

    feat : comment CRUD

--- src/comment/comment.controller.ts
+++ src/comment/comment.controller.ts
@@ -13,8 +13,37 @@ import { CommentService } from './comment.service';
 export class CommentController {
   constructor(private readonly commentService: CommentService) {}
 
-  @Get(':/taskId')
+  @Get('/:taskId')
   async getComments(@Param('taskId') taskId: number) {
     return await this.commentService.findAll(taskId);
+  }
+
+  @Post('/:taskId')
+  async createComment(
+    @Param('taskId') taskId: number,
+    @Body() content: string,
+  ) {
+    const comment = await this.commentService.create(taskId, content);
+
+    if (comment) {
+      return { message: '댓글 생성 완료', comment };
+    }
+  }
+
+  @Patch('/:commentId')
+  async updateComment(
+    @Param('commentId') commentId: number,
+    @Body() content: string,
+  ) {
+    const comment = await this.commentService.update(commentId, content);
+
+    return { message: '댓글 수정 완료', comment };
+  }
+
+  @Delete('/:commentId')
+  async deleteComment(@Param('commentId') commentId: number) {
+    await this.commentService.delete(commentId);
+
+    return { message: '댓글 삭제 완료' };
   }
 }
--- src/comment/comment.service.ts
+++ src/comment/comment.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Comment } from './entities/comment.entity';
@@ -16,5 +16,53 @@ export class CommentService {
         taskId: taskId,
       },
     });
+  }
+
+  async create(taskId: number, content: string) {
+    const comment = await this.commentRepository.save({
+      taskId: taskId,
+      content: content,
+      userId: 1,
+    });
+    return comment;
+  }
+
+  async update(commentId: number, content: string) {
+    const findcomment = await this.commentRepository.find({
+      where: {
+        id: commentId,
+      },
+    });
+
+    if (!findcomment) {
+      throw new NotFoundException('해당 댓글을 찾을 수 없습니다.');
+    }
+
+    if (findcomment[0].id !== commentId) {
+      throw new Error('본인 댓글만 수정할 수 있습니다.');
+    }
+
+    return await this.commentRepository.update(
+      { id: commentId },
+      { content: content },
+    );
+  }
+
+  async delete(commentId: number) {
+    const findcomment = await this.commentRepository.find({
+      where: {
+        id: commentId,
+      },
+    });
+
+    if (!findcomment) {
+      throw new NotFoundException('해당 댓글을 찾을 수 없습니다.');
+    }
+
+    if (findcomment[0].id !== commentId) {
+      throw new Error('본인 댓글만 삭제할 수 있습니다.');
+    }
+
+    await this.commentRepository.delete({ id: commentId });
   }
 }
