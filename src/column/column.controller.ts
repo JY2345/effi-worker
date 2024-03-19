70100eb232d856baa69ad7dcc767a8dbeb25a98f
JY2345 <ilokids@gmail.com>
2024-03-19 16:02:00 +0900

    Feat : BoardColumn 관련 crud

--- src/column/column.controller.ts
+++ src/column/column.controller.ts
@@ -6,23 +6,25 @@ import {
   Patch,
   Param,
   Delete,
+  Query,
 } from '@nestjs/common';
 import { ColumnService } from './column.service';
 import { CreateColumnDto } from './dto/create-column.dto';
 import { UpdateColumnDto } from './dto/update-column.dto';
+import { ColumnEntity } from './entities/column.entity';
 
 @Controller('column')
 export class ColumnController {
   constructor(private readonly columnService: ColumnService) {}
 
-  @Post()
-  create(@Body() createColumnDto: CreateColumnDto) {
-    return this.columnService.create(createColumnDto);
+  @Post('create-column')
+  create(@Body() createColumnDto: CreateColumnDto): Promise<ColumnEntity> {
+    return this.columnService.createColumn(createColumnDto);
   }
 
-  @Get()
-  findAll() {
-    return this.columnService.findAll();
+  @Get('get-all/:boardId')
+  findAllColumnsInBoard(@Param('boardId') boardId: number) {
+    return this.columnService.findAllColumnsInBoard(+boardId);
   }
 
   @Get(':id')
@@ -31,12 +33,12 @@ export class ColumnController {
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateColumnDto: UpdateColumnDto) {
-    return this.columnService.update(+id, updateColumnDto);
+  updateColumnName(@Param('id') id: number, @Body() updateColumnDto: UpdateColumnDto) {
+    return this.columnService.updateColumnName(+id, updateColumnDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.columnService.remove(+id);
+  async removeColumn(@Param('id') id: number) {
+    return this.columnService.removeColumn(+id);
   }
 }
--- src/column/column.module.ts
+++ src/column/column.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { ColumnService } from './column.service';
 import { ColumnController } from './column.controller';
+import { ColumnEntity } from './entities/column.entity';
+import { Board } from '../board/entities/board.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ColumnEntity, Board])],
   controllers: [ColumnController],
   providers: [ColumnService],
+  exports: [TypeOrmModule],
 })
 export class ColumnModule {}
